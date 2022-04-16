17332aa3382e3ecd8a7f0ae463ab46f411ad097a
DennisRono <finnneron1@gmail.com>
2022-04-16 15:22:00 +0300

    lips herd suggest tomorrow officer

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
     try {
       const registration = await navigator.serviceWorker.register(
-        './test-worker.js',
+        './service.js',
         { scope: '/' }
       );
       if (registration.installing) {
