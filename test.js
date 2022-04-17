e38e9314807d3ad56152b2f56564e6199799e57e
DennisRono <finnneron1@gmail.com>
2022-04-17 17:42:01 +0300

    told furniture dear mill purple

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ const getAllFiles = function(dirPath, arrayOfFiles) {
     if (fs.statSync(dirPath + "/" + file).isDirectory()) {
       arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
     } else {
-      arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+      arrayOfFiles.push(dirPath.join("/", file))
     }
   })
 
