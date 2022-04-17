9ecf9e4682fa539ea0f6af8904048a309a487269
DennisRono <finnneron1@gmail.com>
2022-04-17 16:41:01 +0300

    value drop pot original find

--- service-worker.js
+++ service-worker.js
@@ -44,27 +44,6 @@ self.addEventListener("activate", event => {
 });
 
 //offline page
-// self.addEventListener('fetch', (event) => {
-//   if (event.request.mode === 'navigate') {
-//     event.respondWith((async () => {
-//       try {
-//         const preloadResponse = await event.preloadResponse;
-//         if (preloadResponse) {
-//           return preloadResponse;
-//         }
-//         const networkResponse = await fetch(event.request);
-//         return networkResponse;
-//       } catch (error) {
-//         console.log('Fetch failed; returning offline page instead.', error);
-//         const cache = await caches.open(CACHE_NAME);
-//         const cachedResponse = await cache.match(OFFLINE_URL);
-//         return cachedResponse;
-//       }
-//     })());
-//   }
-// });
-
-//offline page
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
     event.respondWith((async () => {
@@ -82,7 +61,7 @@ self.addEventListener('fetch', (event) => {
             const cachedResponse = await cache.match(OFFLINE_URL);
             return cachedResponse;
         } else {
-            const cachresp = await cache.match(currUrl.pathname);
+            const cachresp = await cache.match(event.request);
             return cachresp;
         }
       }
