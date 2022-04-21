04ba5977ae8645c7dfc24e818929112f3daffe29
DennisRono <finnneron1@gmail.com>
2022-04-21 16:35:01 +0300

    although above remove turn jack

--- includes/fileupload.php
+++ includes/fileupload.php
@@ -1,42 +1,19 @@
 <?php
-
-$valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp' , 'pdf' , 'doc' , 'ppt', 'xls', 'docx'); // valid extensions
-$path = 'uploads/'; // upload directory
-
-if(!empty($_POST['name']) || !empty($_POST['email']) || $_FILES['image'])
-{
-$img = $_FILES['image']['name'];
-$tmp = $_FILES['image']['tmp_name'];
-
-// get uploaded file's extension
-$ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
-
-// can upload same image using rand function
-$final_image = rand(1000,1000000).$img;
-
-// check's valid format
-if(in_array($ext, $valid_extensions)) 
-{ 
-$path = $path.strtolower($final_image); 
-
-if(move_uploaded_file($tmp,$path)) 
-{
-echo "<img src='$path' />";
-$name = $_POST['name'];
-$email = $_POST['email'];
-
-//include database configuration file
-include_once 'db.php';
-
-//insert form data in the database
-$insert = $db->query("INSERT uploading (name,email,file_name) VALUES ('".$name."','".$email."','".$path."')");
-
-//echo $insert?'ok':'err';
-}
-} 
-else 
-{
-echo 'invalid';
-}
-}
+    $files = array_filter($_FILES['upload']['name']); //Use something similar before processing files.
+    // Count the number of uploaded files in array
+    $total_count = count($_FILES['upload']['name']);
+    // Loop through every file
+    for( $i=0 ; $i < $total_count ; $i++ ) {
+    //The temp file path is obtained
+    $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
+    //A file path needs to be present
+    if ($tmpFilePath != ""){
+        //Setup our new file path
+        $newFilePath = "./uploadFiles/" . $_FILES['upload']['name'][$i];
+        //File is uploaded to temp dir
+        if(move_uploaded_file($tmpFilePath, $newFilePath)) {
+            //Other code goes here
+        }
+    }
+    }
 ?>
