835164530f8cf412e68012880c41cf5a7f0c62ed
DennisRono <finnneron1@gmail.com>
2022-04-24 09:33:00 +0300

    clean dangerous driver onto has

--- service-worker.js
+++ service-worker.js
@@ -80,7 +80,7 @@ self.addEventListener('activate', function () {
 self.addEventListener('fetch', function (evt) {
   console.log('The service worker is serving the asset.');
 
-  evt.respondWith(fromNetwork(evt.request, 600).catch(function () {
+  evt.respondWith(fromNetwork(evt.request, 6000).catch(function () {
     return fromCache(evt, evt.request);
   }));
 });
