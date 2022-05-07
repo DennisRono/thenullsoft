d6778c83d0d5c5542f239a3fdb23c0541c1b07fd
DennisRono <finnneron1@gmail.com>
2022-05-07 18:31:00 +0300

    first massage some bet generally

--- js/main.js
+++ js/main.js
@@ -26,3 +26,12 @@ const showsearch = () => {
     searchform.classList.add('showsearch')
   }
 }
+
+const shownav = () => {
+  let navigation = document.querySelector('.hnavigation');
+  if(navigation.classList.contains('showmobinav')){
+    navigation.classList.remove('showmobinav')
+  } else {
+    navigation.classList.add('showmobinav')
+  }
+}
