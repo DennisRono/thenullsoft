2cefb23cdf8e4f10127ac88ad23543d45e6d78b6
DennisRono <finnneron1@gmail.com>
2022-05-07 18:16:01 +0300

    shall introduced interior afternoon ordinary

--- index.php
+++ index.php
@@ -40,7 +40,6 @@
                         <li><a href="index.html">Home</a></li>
                         <li><a href="blog.html">Blog</a></li>
                         <li><a href="about.html">About Us</a></li>
-                        
                         <li><a href="contact.html">Contact</a></li>
                     </nav>
                     <div class="hcalltoaction">
