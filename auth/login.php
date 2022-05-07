ca8953d214e23232681ff5ebf85f24f7db666e2f
DennisRono <finnneron1@gmail.com>
2022-05-07 17:14:01 +0300

    shop real interior action indeed

--- auth/login.php
+++ auth/login.php
@@ -156,7 +156,7 @@
     </script>
     <script>
       //get user inputs from forms
-      let email = "<?php if(isset($_POST['email'])){echo $_POST['email'];}else{echo "new";} ?>";
+      let email = "<?php if(isset($_POST['emphone'])){echo $_POST['emphone'];}else{echo "new";} ?>";
       let pass = "<?php if(isset($_POST['password'])){echo $_POST['password'];}else{echo "new";} ?>";
       let cpass = btoa(email+':#:'+pass+':#:login');
     </script>
