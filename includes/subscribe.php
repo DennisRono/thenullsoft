ebb4eb92d1e59b3872e6e48c3448bef1bca0bc45
DennisRono <finnneron1@gmail.com>
2022-05-17 01:06:02 +0300

    mice was current become subject

--- includes/subscribe.php
+++ includes/subscribe.php
@@ -1,5 +1,13 @@
 <?php
     if(isset($_POST['subscribe'])){
         $email = trim($_POST['email']);
+        if(empty($email)){
+            $err = "email cannot be empty!";
+        } else {
+            //save user data to database
+            $stmt = $conn->prepare("INSERT INTO users(Email, Time) values(?, ?)");
+            $stmt->execute([$email, $time]);
+            $stmt=NULL;
+        }
     }
 ?>
