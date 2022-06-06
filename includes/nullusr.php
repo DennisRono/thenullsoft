ed2cd6465414aea71710272c92ef8f4f6b1eecaa
DennisRono <finnneron1@gmail.com>
2022-06-06 12:54:01 +0300

    gas gray several exclaimed poor

--- includes/nullusr.php
+++ includes/nullusr.php
@@ -1,5 +1,5 @@
 <?php
-    $prevdat = (array) file_get_contents("../data/usrdat.json");
+    $prevdat = (array) json_decode(file_get_contents("../data/usrdat.json"));
     $date = new DateTime("now", new DateTimeZone('Africa/Nairobi'));
     $time = $date->format('Y-m-d H:i:s');
     $nusr = array('Time' => $time);
