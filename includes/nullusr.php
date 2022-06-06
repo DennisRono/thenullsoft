a4a5ba9ecc83d0ad540ea8ea2d82ff2dd3166efd
DennisRono <finnneron1@gmail.com>
2022-06-06 12:24:03 +0300

    seems son increase city badly

--- includes/nullusr.php
+++ includes/nullusr.php
@@ -1,3 +1,9 @@
 <?php
-    $jdat = json_encode($data, JSON_PRETTY_PRINT);
+    $name = "dennis";
+    $id = "1";
+    $url = "https://hello.com";
+    $array = array('name' => $name,'id' => $id,'url' => $url);
+    $fp = fopen('../data/usrdat.json', 'w');
+    fwrite($fp, json_encode($array, JSON_PRETTY_PRINT));   // here it will print the array pretty
+    fclose($fp);
 ?>
