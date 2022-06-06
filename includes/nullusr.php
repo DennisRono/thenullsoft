ab497f663ce70c88218515ca87de1a4e8ee570f0
DennisRono <finnneron1@gmail.com>
2022-06-06 12:26:01 +0300

    atomic any shelter trunk like

--- includes/nullusr.php
+++ includes/nullusr.php
@@ -2,6 +2,8 @@
     $name = "dennis";
     $id = "1";
     $url = "https://hello.com";
+    $date = new DateTime("now", new DateTimeZone('Africa/Nairobi'));
+    $time = $date->format('Y-m-d H:i:s');
     $array = array('name' => $name,'id' => $id,'url' => $url);
     $fp = fopen('../data/usrdat.json', 'w');
     fwrite($fp, json_encode($array, JSON_PRETTY_PRINT));   // here it will print the array pretty
