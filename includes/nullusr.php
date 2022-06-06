c8a98361e9491fdef871f417328cc57260babba1
DennisRono <finnneron1@gmail.com>
2022-06-06 12:51:01 +0300

    turn roar plane law six

--- includes/nullusr.php
+++ includes/nullusr.php
@@ -1,13 +1,11 @@
 <?php
     $prevdat = (array) file_get_contents("../data/usrdat.json");
-    array_push($prevdat,"blue","yellow");
+    
     var_dump($prevdat);
-    $name = "dennis";
-    $id = "1";
-    $url = "https://hello.com";
     $date = new DateTime("now", new DateTimeZone('Africa/Nairobi'));
     $time = $date->format('Y-m-d H:i:s');
-    $array = array('name' => $name,'id' => $id,'url' => $url);
+    $array = array('Time' => $name,'id' => $id,'url' => $url);
+    array_push($prevdat,"blue");
     $fp = fopen('../data/usrdat.json', 'w');
     fwrite($fp, json_encode($array, JSON_PRETTY_PRINT));   // here it will print the array pretty
     fclose($fp);
