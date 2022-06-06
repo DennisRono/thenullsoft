44fed73ed9b0037005831e40f06a748f9cd51c3d
DennisRono <finnneron1@gmail.com>
2022-06-06 12:53:01 +0300

    vertical touch gun composition section

--- includes/nullusr.php
+++ includes/nullusr.php
@@ -1,10 +1,8 @@
 <?php
     $prevdat = (array) file_get_contents("../data/usrdat.json");
-    
-    var_dump($prevdat);
     $date = new DateTime("now", new DateTimeZone('Africa/Nairobi'));
     $time = $date->format('Y-m-d H:i:s');
-    $nusr = array('Time' => $name,'id' => $id,'url' => $url);
+    $nusr = array('Time' => $time);
     array_push($prevdat,$nusr);
     $fp = fopen('../data/usrdat.json', 'w');
     fwrite($fp, json_encode($prevdat, JSON_PRETTY_PRINT));   // here it will print the array pretty
